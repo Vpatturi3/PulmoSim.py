de93d5bd04fa4311b6344e0fc5d6432cfa911145
NB670 <neel468167@gmail.com>
2025-09-28 00:11:31 -0400

    airways

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-python ct_to_mesh.py --input "/Users/neelb/Documents/PulmoSim.py/data/manifest-1759009369418/LIDC-IDRI/LIDC-IDRI-0001/01-01-2000-NA-NA-30178/3000566.000000-NA-03192" --out lungs.stl --decimate 1.0
+python ct_to_mesh.py \
+  --input "/Users/neelb/Documents/PulmoSim.py/data/manifest-1759009369418/LIDC-IDRI/LIDC-IDRI-0002/01-01-2000-NA-NA-98329/3000522.000000-NA-04919" \
+  --out lungs.stl \
+  --iso 1.0 \
+  --decimate 0.95 \
+  --smooth-iters 6 \
+  --repair-strong
